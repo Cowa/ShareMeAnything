562ff24cfd00821978d91408410e536c70b717c3
Brice Thomas <brice.p.thomas@gmail.com>
2014-05-01 22:40:55 +0200

    Add multiple pages behavior

--- public/js/sma.js
+++ public/js/sma.js
@@ -1,8 +1,38 @@
-$('.toggle-sb').click(
-	function() {
-		$('.ui.sidebar').sidebar({
-			overlay: true
-		}).sidebar('toggle');
-	}
-);
+function hideAll() {
+	$('#front').hide();
+	$('#favorite').hide();
+	$('#history').hide();
+	$('#settings').hide();
+	$('#about').hide();
+}
 
+$('.toggle-sb').click(function() {
+	$('.ui.sidebar').sidebar({
+		overlay: true
+	}).sidebar('toggle');
+});
+
+$('#history-menu').click(function() {
+	hideAll();
+	$('#history').show();
+});
+
+$('#favorite-menu').click(function() {
+	hideAll();
+	$('#favorite').show();
+});
+
+$('#settings-menu').click(function() {
+	hideAll();
+	$('#settings').show();
+});
+
+$('#about-menu').click(function() {
+	hideAll();
+	$('#about').show();
+});
+
+$('#front-menu').click(function() {
+	hideAll();
+	$('#front').show();
+});
